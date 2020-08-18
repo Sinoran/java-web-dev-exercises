b4d2b7eac6c8553ffd5341122fa808c3e14d719c
Scott Sullentrop <sscalex@yahoo.com>
2020-08-17 20:02:18 -0500

    Studio one finished!

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,27 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.sql.SQLOutput;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        double radius;
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is the radius of your circle?");
+        try {
+            radius = input.nextDouble();
+        } catch(InputMismatchException error) {
+            System.out.println("Please enter a number.");
+            return;
+        }
+        while(radius < 0) {
+            System.out.println("What is the radius of your circle?");
+            radius = input.nextDouble();
+        }
+        double areaOfCircle = Circle.getArea(radius);
+        System.out.println("The area of the circle is " + areaOfCircle);
+        input.close();
+
+    }
+}
